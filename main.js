05477a25587cb348dfa63c99a5ebf6b15e39698d
KalashnikovTV <max.malaschkov@mail.ru>
2021-04-19 23:13:23 +0300

    refactor: change structure of the project, change blur scalling

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const imageCurrent = document.querySelector('.image-current');
+const filterInputs = document.querySelectorAll('.filters input');
 
 // Add active class to the current button
 function activeBtns() {
@@ -14,7 +15,6 @@ activeBtns();
 
 // Filters: blur, invert, sepia, saturate, hue
 function handleFilters() {
-    const inputs = document.querySelectorAll('.filters input');
     const btnReset = document.querySelector('.btn-reset');
 
     function handleUpdate() {
@@ -24,10 +24,10 @@ function handleFilters() {
         const outputs = this.nextElementSibling;
         outputs.innerHTML = this.value;
     }
-    inputs.forEach(input => input.addEventListener('input', handleUpdate));
+    filterInputs.forEach(input => input.addEventListener('input', handleUpdate));
     
     function handleReset() {
-        inputs.forEach(input => {
+        filterInputs.forEach(input => {
             input.name === 'saturate' ? input.value = 100 : input.value = 0;
             imageCurrent.style.setProperty(`--${input.name}`, input.value + (input.dataset.sizing || ''));
     
@@ -48,27 +48,20 @@ function nextPicture() {
         const hour = new Date().getHours();
         if (hour >= 6 && hour < 12) {
             return basePath + 'morning/';
-            // basePath += 'morning/';
         } else if (hour >= 12 && hour < 18) {
             return basePath + 'day/';
-            // basePath += 'day/';
         } else if (hour >= 18 && hour < 24) {
-            return basePath + 'evening/'
-            // basePath += 'evening/';
+            return basePath + 'evening/';
         } else {
-            return basePath + 'night/'
-            // basePath += 'night/';
+            return basePath + 'night/';
         }
     }
-    // getPicturePath();
 
     const imagesList = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
     let i = 0;
 
     function getImage() {
-        // getPicturePath();
         const index = i % imagesList.length;
-        // const imageSrc = basePath + imagesList[index];
         const imageSrc = getPicturePath() + imagesList[index];
         imageCurrent.src = imageSrc;
         i++;
@@ -98,45 +91,36 @@ function loadPicture() {
 }
 loadPicture();
 
-// Download picture with Canvas API
-function downloadPicture() {
+// Draw and save picture with Canvas API
+function drawPicture() {
     const canvas = document.querySelector('canvas');
-    const btnSavePicture = document.querySelector('.btn-save');
-
-    function getFilters() {
-        let filters = '';
-        let imgFilters = imageCurrent.style.cssText;
-
-        for (let i = 0; i < imgFilters.length; i++) {
-            if (imgFilters[i] === "-" && imgFilters[i - 1] !== "e") {
-                continue;
-            } else {
-                switch (imgFilters[i]) {
-                    case ":": 
-                        filters = filters + "(";
-                        break;
-                    case ";": 
-                        filters = filters + ")";
-                        break;
-                    default: 
-                        filters = filters + imgFilters[i];
-                        break;
-                }
-            }
-        }
-
-        return filters;
-    }
+    const ctx = canvas.getContext("2d");
 
     function createCanvas(img) {
-        const ctx = canvas.getContext("2d");
-        canvas.width = img.naturalWidth;
-        canvas.height = img.naturalHeight;
-        ctx.filter = getFilters();
+        let filters = '';
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        filterInputs.forEach(input => {
+            if (input.name === 'blur') {
+                const sizeScalling = input.value * ((img.width / imageCurrent.width + img.height / imageCurrent.height) / 2);
+                filters += `${input.name}(${sizeScalling}${input.dataset.sizing})`;
+                console.log(`Blur: ${filters}`);
+            } else {
+                filters += `${input.name}(${input.value}${input.dataset.sizing})`;
+            }
+        });
+
+        console.log(`Filters: ${filters}`);
+        ctx.filter = filters.trim();
+        // ctx.globalCompositeOperation = "destination-out";
         ctx.drawImage(img, 0, 0);
     }
 
     function savePicture() {
+        const btnSavePicture = document.querySelector('.btn-save');
+
         btnSavePicture.addEventListener('click', () => {
             const img = new Image();
             img.setAttribute('crossOrigin', 'anonymous');
@@ -145,7 +129,7 @@ function downloadPicture() {
                 createCanvas(img);
                 let link = document.createElement('a');
                 link.download = 'picture.png';
-                link.href = canvas.toDataURL();
+                link.href = canvas.toDataURL('image/png');
                 link.click();
                 link.delete;
             };
@@ -153,7 +137,7 @@ function downloadPicture() {
     }
     savePicture();
 }
-downloadPicture();
+drawPicture();
 
 // Fullscreen mode
 function fullscreen() {
